79f24e5c3d12c0c8b369752038758dddf4d0db2a
Bhanuprakash0531 <bhanuprakash45.y@gmail.com>
2025-03-14 15:23:23 -0500

    FetchDemoAysnc file added

---
+++ src/components/FetchDemoAsync.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+
+function FetchDemoAsync(){
+    const[profile,setProfile]=useState(null);
+    useEffect(()=>{
+        async function fetchData(){
+            try{
+                const response = await fetch('http://localhost:3001/api/profile');
+                const data = await response.json();
+                setProfile(data);
+                //console.log('Fetched data(async/await):',data);
+            }catch(error){
+                console.error('errror fetching data:',error);
+            }
+        }
+        fetchData();
+    },[]);
+    return (
+        <div>
+            {profile?(
+                <div>
+                    <p>User:{profile.name}</p>
+                    <p>Role:{profile.role}</p>
+                </div>
+            ):(<p>Loading...</p>)}
+        </div>
+    );
+}
+export default FetchDemoAsync;
